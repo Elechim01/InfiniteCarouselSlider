6d3ff5c3bad8aa7dfe2f9a7edbcfd1044da9cf6d
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-25 22:10:48 +0200

    Initial Commit

---
+++ Shared/InfiniteCarouselSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteCarouselSliderApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 25/09/21.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteCarouselSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
